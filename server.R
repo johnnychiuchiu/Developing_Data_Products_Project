bb9cd033f3d927a91f215eb34c662591e9679479
Homer White <homerhanumat@gmail.com>
2015-07-27 19:51:36 -0400

    offer suggestions to johhnychiuchiu

--- server.R
+++ server.R
@@ -1,20 +1,29 @@
 library(shiny) 
 library(ggplot2) 
 
+result_mean <- NULL
+
 shinyServer(
   function(input, output) {
     output$newHist <- renderPlot({
-      n<-input$n
-      B<-input$B
-      lambda<-input$lambda
-      resamples <- matrix(sample(x, n * B, replace = TRUE), B, n)
-      means <- apply(resamples, 1, mean)
-      result_mean <- mean(means)
-      hist(means,col='lightblue')
-      lines(c(result_mean, result_mean), c(0, 200),col="red",lwd=5)
-      text(60, 150, paste("mean = ", result_mean))
-      text(63, 140, paste("sd = ", result_sd))
-    })  
+      if (input$go > 0) {
+        n<-isolate(input$n)
+        B<-isolate(input$B)
+        lambda<-isolate(input$lambda)
+        resamples <- matrix(rexp(n*B, rate = 1/lambda), B, n)
+        means <- apply(resamples, 1, mean)
+        result_mean <<- mean(means)
+        hist(means,col='lightblue')
+        lines(c(result_mean, result_mean), c(0, 200),col="red",lwd=5)
+        #text(60, 150, paste("mean = ", result_mean))
+        #text(63, 140, paste("sd = ", result_sd))
+      }
+    })
+    output$summary <- renderText({
+      if (input$go > 0) {
+        paste0("The mean of the sample means is ", round(result_mean, 3),".")
+      }
+    })
   } 
 )
 
--- ui.R
+++ ui.R
@@ -4,12 +4,17 @@ library(shiny)
 shinyUI(pageWithSidebar( 
   headerPanel("Illustrating markup"), 
   sidebarPanel(
-    sliderInput('lambda', 'input the lambda of exponential distribution',value = 10, min = 1, max = 20, step = 1,),
-    sliderInput('n', 'input the number of the exponential distribution',value = 40, min = 20, max = 60, step = 1,),
-    sliderInput('B', 'input the time of the Bootstraping',value = 500, min = 100, max = 1000, step = 1,)
+    sliderInput('lambda', 'input the lambda of exponential distribution',
+                value = 10, min = 1, max = 20, step = 1),
+    sliderInput('n', 'input the sample size',
+                value = 5, min = 1, max = 50, step = 1),
+    sliderInput('B', 'input the number of random samples to take',
+                value = 500, min = 100, max = 1000,step = 1),
+    actionButton("go","Simulate Now")
        ), 
   mainPanel(
-    plotOutput('newHist')
+    plotOutput('newHist'),
+    textOutput("summary")
   ) 
 ))
 
